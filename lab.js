74aa66e83c9f24cb0ff72888f228ebe7dcfd00c0
benahahumber <n01713464@humber.ca>
2024-06-13 19:51:09 -0400

    finished with notes

--- lab.js
+++ lab.js
@@ -1,18 +1,18 @@
-var myNumber = 10
-var myString = "Hello, Javascript!"
-console.log(myNumber)
+var myNumber = 10 // seting a variable as 10 
+var myString = "Hello, Javascript!" // setting a variable as a string of textx
+console.log(myNumber) // printing in the console the var
 console.log(myString)
-for (let i = 0 ; i <10; i++)
-    console.log(i)
-for (let n=0; n <4; n++ )
-    console.log(3*n)
-function addnumbers(num1, num2) {
-    return num1 + num2;    
+for (let i = 0 ; i <10; i++)// start of the for loop, i var to start, for loop is run ten times, increase by one
+    console.log(i) // print i
+for (let n=1; n <4; n++ ) // for loop, start with one and go up to 3,
+    console.log(3*n)// for each n, multiply it by three, return in console
+function addnumbers(num1, num2) { // function name and arguments 
+    return num1 + num2;    // return
 }
-console.log(addnumbers(69, 420));
+console.log(addnumbers(69, 420)); // print to console the function and add inputs
 
-function multiplyNumbers(num1, num2) {
-    return num1*num2
+function multiplyNumbers(num1, num2) { // function name an arguments
+    return num1*num2 //return 
 }
     
-console.log(multiplyNumbers(6, 12));
+console.log(multiplyNumbers(6, 12)); // print to console the function and add inputs
